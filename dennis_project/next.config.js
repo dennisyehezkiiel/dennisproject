98dbf69143541d12f2e1700db3d1375faae190ed
Dennis Yehezkiel <dennisyehezkiel.m@gmail.com>
2022-05-04 09:06:42 +0700

    first push

--- dennis_project/next.config.js
+++ dennis_project/next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  images: {
+    loader: 'custom'
+  },
+}
+
+// module.exports = nextConfig
